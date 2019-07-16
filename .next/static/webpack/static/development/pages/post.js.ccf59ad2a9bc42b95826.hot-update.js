f6dd527952d839867bea8e61348dbea4f3b233e7
maruniaks <st.maruniak@gmail.com>
2019-07-16 15:55:31 +0300

    first commit

---
+++ .next/static/webpack/static/development/pages/post.js.ccf59ad2a9bc42b95826.hot-update.js
@@ -0,0 +1,56 @@
+webpackHotUpdate("static/development/pages/post.js",{
+
+/***/ "./node_modules/isomorphic-unfetch/browser.js":
+false,
+
+/***/ "./node_modules/unfetch/dist/unfetch.mjs":
+false,
+
+/***/ "./pages/post.js":
+/*!***********************!*\
+  !*** ./pages/post.js ***!
+  \***********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/router */ "./node_modules/next/dist/client/router.js");
+/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_router__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components_Layout_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../components/Layout.js */ "./components/Layout.js");
+var _jsxFileName = "/Users/maruniaks/MyProjects/react-next/pages/post.js";
+
+
+
+
+var Page = function Page() {
+  var router = Object(next_router__WEBPACK_IMPORTED_MODULE_1__["useRouter"])();
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Layout_js__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, router.query.title), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, "This is the blog post content."));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Page);
+
+/***/ })
+
+})
+//# sourceMappingURL=post.js.ccf59ad2a9bc42b95826.hot-update.js.map
